503a22b28216481065cea7aba3e4c5f7cf55d68d
Divyanshu Urmaliya <urmaliyadiv04@gmail.com>
2022-12-12 00:49:26 +0530

    Added Initial Start

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,15 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1 className='text-test'>GEAR UP < br/> YOUR NFT < br/> PURCHASE!</h1>
+      
+      <button className='btn-grad'>BUY NFT</button>
+      <button className='btn-grad'>Finance</button>
+       <div className="dark-bg">
+        
+        <div className="gradient-bg"></div>
+        
+      </div>
     </div>
   )
 }
